c3d9977e0fe729774843f5e1365406667dbc8c90
jeremy <jeremy_tan@sla.gov.sg>
2016-12-09 18:04:49 +0800

    Added way to load multiple markers and disable the hiding of info window

--- app/src/main/java/example/mapbox/sla/mapboxdemo/MainActivity.java
+++ app/src/main/java/example/mapbox/sla/mapboxdemo/MainActivity.java
@@ -8,8 +8,10 @@
 
 package example.mapbox.sla.mapboxdemo;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
@@ -40,7 +42,20 @@ public class MainActivity extends FragmentActivity {
         //Callback when map finish loading and is ready to be used
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onMapReady(MapboxMap mapboxMap) {
+            public void onMapReady(final MapboxMap mapboxMap) {
+
+
+                //region CUSTOM MARKER INFO WINDOW
+                //Comment out to use back default marker info window
+                CustomMarkerInfoWindow customMarkerInfoWindow = new CustomMarkerInfoWindow(MainActivity.this);
+                mapboxMap.setInfoWindowAdapter(customMarkerInfoWindow);
+                mapboxMap.setOnInfoWindowCloseListener(new MapboxMap.OnInfoWindowCloseListener() {
+                    @Override
+                    public void onInfoWindowClose(Marker marker) {
+
+                    }
+                });
+                //endregion
 
                 //region ZOOM TO LOCATION
                 LatLng zoomLocation = new LatLng(1.358479,103.815201);
@@ -53,19 +68,40 @@ public class MainActivity extends FragmentActivity {
                 //endregion
 
                 //region ADD MARKER
-                MarkerOptions markerOptions = new MarkerOptions();
-                markerOptions.position(zoomLocation).title("Marker Title").snippet("This is a marker snippet!");
-                mapboxMap.addMarker(markerOptions);
+                mapboxMap.setAllowConcurrentMultipleOpenInfoWindows(true); //Enable concurrent opening of marker info window
+                mapboxMap.getUiSettings().setDeselectMarkersOnTap(false);  //Control hiding of marker info window on map tap
+
+                MarkerOptions markerOptions1 = new MarkerOptions();
+                markerOptions1.position(zoomLocation).title("Marker 1").snippet("Marker snippet 1!");
+                final Marker marker1 = mapboxMap.addMarker(markerOptions1);
+                mapboxMap.selectMarker(marker1);
+
+                MarkerOptions markerOptions2 = new MarkerOptions();
+                markerOptions2.position( new LatLng(1.358479,103.715201)).title("Marker 2").snippet("Marker snippet 2!");
+                final Marker marker2 = mapboxMap.addMarker(markerOptions2);
+                mapboxMap.selectMarker(marker2);
                 //endregion
 
-                //region CUSTOM MARKER INFO WINDOW
-                //Comment out to use back default marker info window
-                CustomMarkerInfoWindow customMarkerInfoWindow = new CustomMarkerInfoWindow(MainActivity.this);
-                mapboxMap.setInfoWindowAdapter(customMarkerInfoWindow);
-                mapboxMap.setOnInfoWindowCloseListener(new MapboxMap.OnInfoWindowCloseListener() {
+                //region mapboxMap event handlers
+                mapboxMap.setOnMarkerClickListener(new MapboxMap.OnMarkerClickListener() {
                     @Override
-                    public void onInfoWindowClose(Marker marker) {
-
+                    public boolean onMarkerClick(@NonNull Marker marker) {
+                        Log.i("TEST", "On marker clicked");
+                        //NOTE: Will not be fired if marker info window is shown.
+                        return false;
+                    }
+                });
+                mapboxMap.setOnInfoWindowClickListener(new MapboxMap.OnInfoWindowClickListener() {
+                    @Override
+                    public boolean onInfoWindowClick(@NonNull Marker marker) {
+                        Log.i("TEST", "On marker info window clicked");
+                        return true; //Set to true to disable hiding of info window when clicked
+                    }
+                });
+                mapboxMap.setOnMapLongClickListener(new MapboxMap.OnMapLongClickListener() {
+                    @Override
+                    public void onMapLongClick(@NonNull LatLng point) {
+                        //Handles long click event on app
                     }
                 });
                 //endregion
