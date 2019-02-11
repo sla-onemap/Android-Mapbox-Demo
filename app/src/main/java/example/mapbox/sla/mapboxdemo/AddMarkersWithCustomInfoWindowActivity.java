b8ceb036bb7a9af6d954b27ee3d17588e55d52cf
joyce <joyceleong.sla@gmail.com>
2019-02-11 12:06:01 +0800

    Refactor 'Custom marker info window' Remove custom info window Add custom dialog UI

--- app/src/main/java/example/mapbox/sla/mapboxdemo/AddMarkersWithCustomInfoWindowActivity.java
+++ app/src/main/java/example/mapbox/sla/mapboxdemo/AddMarkersWithCustomInfoWindowActivity.java
@@ -1,21 +1,57 @@
 package example.mapbox.sla.mapboxdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PointF;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import com.mapbox.mapboxsdk.annotations.Marker;
-import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
-public class AddMarkersWithCustomInfoWindowActivity extends AppCompatActivity {
-    // MapBox
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static com.mapbox.mapboxsdk.style.expressions.Expression.eq;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.get;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.literal;
+import static com.mapbox.mapboxsdk.style.layers.Property.ICON_ANCHOR_BOTTOM;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAnchor;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
+
+public class AddMarkersWithCustomInfoWindowActivity extends AppCompatActivity implements
+        OnMapReadyCallback, MapboxMap.OnMapClickListener {
     private MapView mapView;
+    private MapboxMap mapboxMap;
+    private GeoJsonSource source;
+    private FeatureCollection featureCollection;
+
+    private static final String GEOJSON_SOURCE_ID = "GEOJSON_SOURCE_ID";
+    private static final String MARKER_IMAGE_ID = "MARKER_IMAGE_ID";
+    private static final String MARKER_LAYER_ID = "MARKER_LAYER_ID";
+    private static final String CALLOUT_LAYER_ID = "CALLOUT_LAYER_ID";
+    private static final String MARKER_SELECTED = "selected";
+    private static final String MARKER_DESCRIPTION = "description";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,61 +63,342 @@ public class AddMarkersWithCustomInfoWindowActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
+        // refer to https://docs.mapbox.com/android/maps/examples/symbol-layer-info-window/
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.onCreate(savedInstanceState); //This is essential for mapbox to work
-        mapView.getMapAsync(new OnMapReadyCallback() {
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(Constants.DEFAULT_BASEMAP_URL, new Style.OnStyleLoaded() {
             @Override
-            public void onMapReady(@NonNull final MapboxMap mapboxMap) {
-                mapboxMap.setStyle(Constants.DEFAULT_BASEMAP_URL, new Style.OnStyleLoaded() { //SET CUSTOM BASE MAP URL
-                    @Override
-                    public void onStyleLoaded(@NonNull Style style) {
-                        //region CUSTOM MARKER INFO WINDOW
-                        CustomMarkerInfoWindow customMarkerInfoWindow = new CustomMarkerInfoWindow(AddMarkersWithCustomInfoWindowActivity.this);
-                        mapboxMap.setInfoWindowAdapter(customMarkerInfoWindow);
-                        mapboxMap.setOnInfoWindowCloseListener(new MapboxMap.OnInfoWindowCloseListener() {
-                            @Override
-                            public void onInfoWindowClose(Marker marker) {
-
-                            }
-                        });
-                        //endregion
-
-                        //region ADD MARKERS
-                        mapboxMap.setAllowConcurrentMultipleOpenInfoWindows(true); //Enable concurrent opening of marker info window
-                        mapboxMap.getUiSettings().setDeselectMarkersOnTap(false);  //Control hiding of marker info window on map tap
-
-                        MarkerOptions markerOptions1 = new MarkerOptions();
-                        markerOptions1.position(new LatLng(1.34, 103.82)).title("Marker 1").snippet("Marker snippet 1!");
-                        final Marker marker1 = mapboxMap.addMarker(markerOptions1);
-                        mapboxMap.selectMarker(marker1);
-
-                        MarkerOptions markerOptions2 = new MarkerOptions();
-                        markerOptions2.position(new LatLng(1.35, 103.72)).title("Marker 2").snippet("Marker snippet 2!");
-                        final Marker marker2 = mapboxMap.addMarker(markerOptions2);
-                        mapboxMap.selectMarker(marker2);
-                        //endregion
-
-                        //region mapboxMap event handlers
-                        mapboxMap.setOnMarkerClickListener(new MapboxMap.OnMarkerClickListener() {
-                            @Override
-                            public boolean onMarkerClick(@NonNull Marker marker) {
-                                Log.i("TEST", "On marker clicked");
-                                //NOTE: Will not be fired if marker info window is shown.
-                                return false;
-                            }
-                        });
-                        mapboxMap.setOnInfoWindowClickListener(new MapboxMap.OnInfoWindowClickListener() {
-                            @Override
-                            public boolean onInfoWindowClick(@NonNull Marker marker) {
-                                Log.i("TEST", "On marker info window clicked");
-                                return true; //Set to true to disable hiding of info window when clicked
-                            }
-                        });
-                        //endregion
-                    }
-                });
+            public void onStyleLoaded(@NonNull Style style) {
+                new LoadGeoJsonDataTask(AddMarkersWithCustomInfoWindowActivity.this).execute();
+                mapboxMap.addOnMapClickListener(AddMarkersWithCustomInfoWindowActivity.this);
             }
         });
+    }
+
+    @Override
+    public boolean onMapClick(@NonNull LatLng point) {
+        return handleClickIcon(mapboxMap.getProjection().toScreenLocation(point));
+    }
+
+    /**
+     * Sets up all of the sources and layers needed for this example
+     *
+     * @param collection the FeatureCollection to set equal to the globally-declared FeatureCollection
+     */
+    public void setUpData(final FeatureCollection collection) {
+        featureCollection = collection;
+        if (mapboxMap != null) {
+            Style style = mapboxMap.getStyle();
+
+            if (style != null) {
+                setupSource(style);
+                setUpImage(style);
+                setUpMarkerLayer(style);
+                setUpInfoWindowLayer(style);
+            }
+        }
+    }
+
+    /**
+     * Adds the GeoJSON source to the map
+     */
+    private void setupSource(@NonNull Style loadedStyle) {
+        source = new GeoJsonSource(GEOJSON_SOURCE_ID, featureCollection);
+        loadedStyle.addSource(source);
+    }
+
+    /**
+     * Adds the marker image to the map for use as a SymbolLayer icon
+     */
+    private void setUpImage(@NonNull Style loadedStyle) {
+        loadedStyle.addImage(MARKER_IMAGE_ID, BitmapFactory.decodeResource(
+                this.getResources(), R.drawable.mapbox_marker_icon_default));
+    }
+
+    /**
+     * Updates the display of data on the map after the FeatureCollection has been modified
+     */
+    private void refreshSource() {
+        if (source != null && featureCollection != null) {
+            source.setGeoJson(featureCollection);
+        }
+    }
+
+    /**
+     * Setup a layer with maki icons, eg. west coast city.
+     */
+    private void setUpMarkerLayer(@NonNull Style loadedStyle) {
+        loadedStyle.addLayer(new SymbolLayer(MARKER_LAYER_ID, GEOJSON_SOURCE_ID)
+                .withProperties(
+                        iconImage(MARKER_IMAGE_ID),
+                        iconAllowOverlap(true)
+                ));
+    }
+
+    /**
+     * Setup a layer with Android SDK call-outs
+     * <p>
+     * name of the feature is used as key for the iconImage
+     * </p>
+     */
+    private void setUpInfoWindowLayer(@NonNull Style loadedStyle) {
+        loadedStyle.addLayer(new SymbolLayer(CALLOUT_LAYER_ID, GEOJSON_SOURCE_ID)
+                .withProperties(
+                        /* show image with id title based on the value of the name feature property */
+                        iconImage("{name}"),
+
+                        /* set anchor of icon to bottom-left */
+                        iconAnchor(ICON_ANCHOR_BOTTOM),
+
+                        /* all info window and marker image to appear at the same time*/
+                        iconAllowOverlap(true),
+
+                        /* offset the info window to be above the marker */
+                        iconOffset(new Float[]{-2f, -25f})
+                )
+                /* add a filter to show only when selected feature property is true */
+                .withFilter(eq((get(MARKER_SELECTED)), literal(true))));
+    }
+
+    /**
+     * This method handles click events for SymbolLayer symbols.
+     * <p>
+     * When a SymbolLayer icon is clicked, we moved that feature to the selected state.
+     * </p>
+     *
+     * @param screenPoint the point on screen clicked
+     */
+    private boolean handleClickIcon(PointF screenPoint) {
+        List<Feature> features = mapboxMap.queryRenderedFeatures(screenPoint, MARKER_LAYER_ID);
+
+        if (!features.isEmpty()) {
+            String name = features.get(0).getStringProperty(MARKER_DESCRIPTION);
+            List<Feature> featureList = featureCollection.features();
+
+            for (int i = 0; i < featureList.size(); i++) {
+                if (featureList.get(i).getStringProperty(MARKER_DESCRIPTION).equals(name)) {
+                    if (featureSelectStatus(i)) {
+                        setFeatureSelectState(featureList.get(i), false);
+                    } else {
+                        setSelected(i);
+                    }
+                }
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Set a feature selected state.
+     *
+     * @param index the index of selected feature
+     */
+    private void setSelected(int index) {
+        Feature feature = featureCollection.features().get(index);
+        setFeatureSelectState(feature, true);
+        refreshSource();
+    }
+
+    /**
+     * Selects the state of a feature
+     *
+     * @param feature the feature to be selected.
+     */
+    private void setFeatureSelectState(Feature feature, boolean selectedState) {
+        feature.properties().addProperty(MARKER_SELECTED, selectedState);
+        refreshSource();
+    }
+
+    /**
+     * Checks whether a Feature's boolean "selected" property is true or false
+     *
+     * @param index the specific Feature's index position in the FeatureCollection's list of Features.
+     * @return true if "selected" is true. False if the boolean property is false.
+     */
+    private boolean featureSelectStatus(int index) {
+        if (featureCollection == null) {
+            return false;
+        }
+
+        return featureCollection.features().get(index).getBooleanProperty(MARKER_SELECTED);
+    }
+
+    /**
+     * Invoked when the bitmaps have been generated from a view.
+     */
+    public void setImageGenResults(HashMap<String, Bitmap> imageMap) {
+        if (mapboxMap != null) {
+            Style style = mapboxMap.getStyle();
+
+            if (style != null) {
+                // calling addImages is faster as separate addImage calls for each bitmap.
+                style.addImages(imageMap);
+            }
+        }
+    }
+
+    /**
+     * AsyncTask to load data from the assets folder.
+     */
+    private static class LoadGeoJsonDataTask extends AsyncTask<Void, Void, FeatureCollection> {
+
+        private final WeakReference<AddMarkersWithCustomInfoWindowActivity> activityRef;
+
+        LoadGeoJsonDataTask(AddMarkersWithCustomInfoWindowActivity activity) {
+            this.activityRef = new WeakReference<>(activity);
+        }
+
+        @Override
+        protected FeatureCollection doInBackground(Void... params) {
+            AddMarkersWithCustomInfoWindowActivity activity = activityRef.get();
+            List<Feature> features = new ArrayList<>();
+
+            if (activity == null) {
+                return null;
+            }
+
+            Feature feature1 = Feature.fromGeometry(Point.fromLngLat(103.82, 1.34));
+            Feature feature2 = Feature.fromGeometry(Point.fromLngLat(103.72, 1.35));
+
+            feature1.addStringProperty("description", "Marker 1");
+            feature2.addStringProperty("description", "Marker 2");
+
+            features.add(feature1);
+            features.add(feature2);
+
+            return FeatureCollection.fromFeatures(features);
+        }
+
+        @Override
+        protected void onPostExecute(FeatureCollection featureCollection) {
+            super.onPostExecute(featureCollection);
+
+            AddMarkersWithCustomInfoWindowActivity activity = activityRef.get();
+
+            if (featureCollection == null || activity == null) {
+                return;
+            }
+
+            // This example runs on the premise that each GeoJSON Feature has a "selected" property,
+            // with a boolean value. If your data's Features don't have this boolean property,
+            // add it to the FeatureCollection 's features with the following code:
+            for (Feature singleFeature : featureCollection.features()) {
+                singleFeature.addBooleanProperty(MARKER_SELECTED, false);
+            }
+
+            activity.setUpData(featureCollection);
+
+            new GenerateViewIconTask(activity).execute(featureCollection);
+        }
+    }
+
+    /**
+     * AsyncTask to generate Bitmap from Views to be used as iconImage in a SymbolLayer.
+     * <p>
+     * Call be optionally be called to update the underlying data source after execution.
+     * </p>
+     * <p>
+     * Generating Views on background thread since we are not going to be adding them to the view hierarchy.
+     * </p>
+     */
+    private static class GenerateViewIconTask extends AsyncTask<FeatureCollection, Void, HashMap<String, Bitmap>> {
+        private final HashMap<String, View> viewMap = new HashMap<>();
+        private final WeakReference<AddMarkersWithCustomInfoWindowActivity> activityRef;
+        private final boolean refreshSource;
+
+        GenerateViewIconTask(AddMarkersWithCustomInfoWindowActivity activity, boolean refreshSource) {
+            this.activityRef = new WeakReference<>(activity);
+            this.refreshSource = refreshSource;
+        }
+
+        GenerateViewIconTask(AddMarkersWithCustomInfoWindowActivity activity) {
+            this(activity, false);
+        }
+
+        @SuppressWarnings("WrongThread")
+        @Override
+        protected HashMap<String, Bitmap> doInBackground(FeatureCollection... params) {
+            AddMarkersWithCustomInfoWindowActivity activity = activityRef.get();
+            if (activity != null) {
+                HashMap<String, Bitmap> imagesMap = new HashMap<>();
+                LayoutInflater inflater = LayoutInflater.from(activity);
+
+                FeatureCollection featureCollection = params[0];
+
+                for (Feature feature : featureCollection.features()) {
+                    LinearLayout linearLayout = (LinearLayout)
+                            inflater.inflate(R.layout.custom_marker_info_window, null);
+
+                    String name = feature.getStringProperty(MARKER_DESCRIPTION);
+                    TextView titleTextView = linearLayout.findViewById(R.id.tv_marker_title);
+                    titleTextView.setText(name);
+
+                    int measureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+                    linearLayout.measure(measureSpec, measureSpec);
+
+                    Bitmap bitmap = SymbolGenerator.generate(linearLayout);
+                    imagesMap.put(name, bitmap);
+                    viewMap.put(name, linearLayout);
+                }
+
+                return imagesMap;
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(HashMap<String, Bitmap> bitmapHashMap) {
+            super.onPostExecute(bitmapHashMap);
+
+            AddMarkersWithCustomInfoWindowActivity activity = activityRef.get();
+
+            if (activity != null && bitmapHashMap != null) {
+                activity.setImageGenResults(bitmapHashMap);
+
+                if (refreshSource) {
+                    activity.refreshSource();
+                }
+            }
+        }
+    }
+
+    /**
+     * Utility class to generate Bitmaps for Symbol.
+     */
+    private static class SymbolGenerator {
+        /**
+         * Generate a Bitmap from an Android SDK View.
+         *
+         * @param view the View to be drawn to a Bitmap
+         * @return the generated bitmap
+         */
+        static Bitmap generate(@NonNull View view) {
+            int measureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+            view.measure(measureSpec, measureSpec);
+
+            int measuredWidth = view.getMeasuredWidth();
+            int measuredHeight = view.getMeasuredHeight();
+
+            view.layout(0, 0, measuredWidth, measuredHeight);
+            Bitmap bitmap = Bitmap.createBitmap(measuredWidth, measuredHeight, Bitmap.Config.ARGB_8888);
+            bitmap.eraseColor(Color.TRANSPARENT);
+
+            Canvas canvas = new Canvas(bitmap);
+
+            view.draw(canvas);
+
+            return bitmap;
+        }
     }
 
     //........................................
@@ -90,7 +407,9 @@ public class AddMarkersWithCustomInfoWindowActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mapView.onDestroy();
+        if (mapboxMap != null) {
+            mapboxMap.removeOnMapClickListener(this);
+        }
     }
 
     @Override
